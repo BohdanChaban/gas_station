76954ab6668ca634ac05c8395f980497f4e34a75
Bohdan Chaban <bohdan.chaban@itv.com>
2018-08-15 15:51:20 +0300

    Add colors for columns

--- column.rb
+++ column.rb
@@ -1,10 +1,11 @@
 require 'ruby-progressbar'
 
 class Column
-  attr_accessor :number
+  attr_accessor :number, :color
 
   def initialize(number)
     @number = number
+    @color = 33 + number
   end
 
   def refuel(car)
@@ -12,7 +13,7 @@ class Column
     (car.gas_limit - car.gas_level).times do
       car.gas_level += 1
       progress.increment
-      sleep 0.01
+      sleep 0.1
     end
     print("\n")
   end
@@ -20,6 +21,7 @@ class Column
   def create_progress(car)
     ProgressBar.create(
       title: "Car##{car.number}(#{car.gas_level}/#{car.gas_limit}L) at Column##{@number}",
+      format: "%t: |\e[0;#{color}m%B\e[0m|",
       starting_at: car.gas_level,
       total: car.gas_limit
     )
